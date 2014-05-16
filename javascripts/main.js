c25a04d4b5ab8ff7ec5eb44942e186aa18bd2de3
Diogo Cunha <diogo.f.cunha@gmail.com>
2014-05-16 14:42:59 +0100

    gallery updated

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,6 +8,32 @@ $(document).ready(function() {
       src: 'images/guinness/guinness3.png'
     }, {
       src: 'images/guinness/guinness4.png'
+    }, {
+      src: 'images/guinness/guinness5.png'
+    }, {
+      src: 'images/guinness/guinness6.png'
+    }, {
+      src: 'images/guinness/guinness7.png'
+    }, {
+      src: 'images/guinness/guinness8.png'
+    }],
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true
+    },
+    type: 'image' // this is default type
+  });
+  $('#base-shots').magnificPopup({
+    items: [{
+      src: 'images/base/base1.png'
+    }, {
+      src: 'images/base/base2.png'
+    }, {
+      src: 'images/base/base3.png'
+    }, {
+      src: 'images/base/base4.png'
+    }, {
+      src: 'images/base/base5.png'
     }],
     gallery: {
       enabled: true,
